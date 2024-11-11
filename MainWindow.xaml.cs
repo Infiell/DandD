080d90147e886797f6e19e6b3e46f38333b32322
Максим Роготнев <30868449+Infiell@users.noreply.github.com>
2024-11-11 15:37:57 +0400

    LVL Change code
    Добавил немного взаимодействие с уровнями

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DandD.Home;
 
 namespace DandD
 {
@@ -27,7 +18,7 @@ namespace DandD
 
         private void Button_Charisma(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void Button_Wisdom(object sender, RoutedEventArgs e)
@@ -57,7 +48,18 @@ namespace DandD
 
         private void Button_LVL(object sender, RoutedEventArgs e)
         {
-
+            var levl = new DandD.Home.PointsGet();
+            try
+            {
+                int Level = Convert.ToInt32(LevelText.Text);
+                int Points = Convert.ToInt32(PointsText.Text);
+                LevelText.Text = Convert.ToString(PointsGet.LevelChange(Level));
+                PointsText.Text = Convert.ToString(PointsGet.PointsChange(Points, Level));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Проверьте формат ввода. Ошибка: {ex}");
+            }
         }
 
         private void Button_PDF(object sender, RoutedEventArgs e)
@@ -72,14 +74,15 @@ namespace DandD
 
         private void ComboBox_CharChange(object sender, SelectionChangedEventArgs e)
         {
-            switch (CharSelect.SelectedIndex) {
-                case 0: ImageChar.Source = new BitmapImage(new Uri("pack://application:,,,/Reference/charAssasin.png")); break;
-                case 1: ImageChar.Source = new BitmapImage(new Uri("pack://application:,,,/Reference/charMage.png")); break;
-                case 2: ImageChar.Source = new BitmapImage(new Uri("pack://application:,,,/Reference/charHealer.png")); break;
-                case 3: ImageChar.Source = new BitmapImage(new Uri("pack://application:,,,/Reference/charTank.png")); break;
-                case 4: ImageChar.Source = new BitmapImage(new Uri("pack://application:,,,/Reference/charWarrior.png")); break;
-                    default: ImageChar.Source = new BitmapImage(new Uri("pack://application:,,,/Reference/charAssasin.png")); break;
-        }
+            ImageChar.Source = CharSelect.SelectedIndex switch
+            {
+                0 => new BitmapImage(new Uri("pack://application:,,,/Reference/charAssasin.png")),
+                1 => new BitmapImage(new Uri("pack://application:,,,/Reference/charMage.png")),
+                2 => new BitmapImage(new Uri("pack://application:,,,/Reference/charHealer.png")),
+                3 => new BitmapImage(new Uri("pack://application:,,,/Reference/charTank.png")),
+                4 => new BitmapImage(new Uri("pack://application:,,,/Reference/charWarrior.png")),
+                _ => new BitmapImage(new Uri("pack://application:,,,/Reference/charAssasin.png")),
+            };
         }
     }
 }
